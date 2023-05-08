62a97a9af35b0283b4a5eb6a4fda052502450055
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-05-08 16:49:55 -0700

    Add: `HashIndex` for Python

--- python/lib.cpp
+++ python/lib.cpp
@@ -24,16 +24,40 @@ using namespace unum::usearch;
 using namespace unum;
 
 namespace py = pybind11;
+using py_shape_t = py::array::ShapeContainer;
 
 using label_t = Py_ssize_t;
 using distance_t = punned_distance_t;
+using id_t = std::uint32_t;
 using native_index_t = auto_index_gt<label_t>;
 
 using set_member_t = std::uint32_t;
 using set_view_t = span_gt<set_member_t const>;
-using sets_index_t = index_gt<jaccard_gt<set_member_t>, label_t, std::uint32_t, set_member_t>;
+using sets_index_t = index_gt<jaccard_gt<set_member_t>, label_t, id_t, set_member_t>;
 
-using hash_index_t = index_gt<bit_hamming_gt<std::uint32_t>, label_t, std::uint32_t, std::size_t>;
+using hash_word_t = std::uint64_t;
+using hash_index_t = index_gt<bit_hamming_gt<hash_word_t>, label_t, id_t, hash_word_t>;
+static constexpr std::size_t bits_per_hash_word_k = sizeof(hash_word_t) * CHAR_BIT;
+
+struct hash_index_w_meta_t : public hash_index_t {
+    using hash_index_t::add;
+    using hash_index_t::capacity;
+    using hash_index_t::reserve;
+    using hash_index_t::search;
+    using hash_index_t::size;
+
+    std::vector<hash_word_t> buffer_;
+    std::size_t words_;
+    std::size_t bits_;
+
+    hash_index_w_meta_t(config_t config, std::size_t bits) : hash_index_t(config) {
+        words_ = divide_round_up<bits_per_hash_word_k>(bits);
+        bits_ = words_ * bits_per_hash_word_k;
+        buffer_.resize(words_);
+    }
+
+    span_gt<hash_word_t const> buffer() const noexcept { return {buffer_.data(), buffer_.size()}; }
+};
 
 static native_index_t make_index(   //
     std::size_t dimensions,         //
@@ -77,6 +101,24 @@ static std::unique_ptr<sets_index_t> make_sets_index( //
     config.max_threads_search = 1;
 
     return std::unique_ptr<sets_index_t>(new sets_index_t(config));
+}
+
+static std::unique_ptr<hash_index_w_meta_t> make_hash_index( //
+    std::size_t bits,                                        //
+    std::size_t capacity,                                    //
+    std::size_t connectivity,                                //
+    std::size_t expansion_add,                               //
+    std::size_t expansion_search                             //
+) {
+    config_t config;
+    config.expansion_add = expansion_add;
+    config.expansion_search = expansion_search;
+    config.connectivity = connectivity;
+    config.max_elements = capacity;
+    config.max_threads_add = 1;
+    config.max_threads_search = 1;
+
+    return std::unique_ptr<hash_index_w_meta_t>(new hash_index_w_meta_t(config, bits));
 }
 
 static void add_one_to_index(native_index_t& index, label_t label, py::buffer vector, bool copy) {
@@ -184,8 +226,8 @@ static py::tuple search_one_in_index(native_index_t& index, py::buffer vector, s
     else
         throw std::invalid_argument("Incompatible scalars in the query vector!");
 
-    labels_py.resize({static_cast<ssize_t>(count)});
-    distances_py.resize({static_cast<ssize_t>(count)});
+    labels_py.resize(py_shape_t{static_cast<ssize_t>(count)});
+    distances_py.resize(py_shape_t{static_cast<ssize_t>(count)});
 
     py::tuple results(3);
     results[0] = labels_py;
@@ -256,24 +298,77 @@ static py::tuple search_many_in_index(native_index_t& index, py::buffer vectors,
     return results;
 }
 
-template <typename index_at = native_index_t> //
-static void save_index(index_at const& index, std::string const& path) {
-    index.save(path.c_str());
+// clang-format off
+template <typename index_at> void save_index(index_at const& index, std::string const& path) { index.save(path.c_str()); }
+template <typename index_at> void load_index(index_at& index, std::string const& path) { index.load(path.c_str()); }
+template <typename index_at> void view_index(index_at& index, std::string const& path) { index.view(path.c_str()); }
+template <typename index_at> void clear_index(index_at& index) { index.clear(); }
+// clang-format on
+
+template <typename element_at> bool has_duplicates(element_at const* begin, element_at const* end) {
+    if (begin == end)
+        return false;
+    element_at const* last = begin;
+    begin++;
+    for (; begin != end; ++begin, ++last) {
+        if (*begin == *last)
+            return true;
+    }
+    return false;
 }
 
-template <typename index_at = native_index_t> //
-static void load_index(index_at& index, std::string const& path) {
-    index.load(path.c_str());
+void validate_set(py::array_t<set_member_t> const& set) {
+    if (set.ndim() != 1)
+        throw std::invalid_argument("Set can't be multi-dimensional!");
+    if (set.strides(0) != sizeof(set_member_t))
+        throw std::invalid_argument("Set can't be strided!");
+    auto proxy = set.unchecked<1>();
+    set_member_t const* begin = &proxy(0);
+    set_member_t const* end = begin + proxy.size();
+    if (!std::is_sorted(begin, end))
+        throw std::invalid_argument("Set must be sorted!");
+    if (has_duplicates(begin, end))
+        throw std::invalid_argument("Set must be deduplicated!");
 }
 
-template <typename index_at = native_index_t> //
-static void view_index(index_at& index, std::string const& path) {
-    index.view(path.c_str());
+inline std::uint64_t hash_ror64(std::uint64_t v, int r) noexcept { return (v >> r) | (v << (64 - r)); }
+
+inline std::uint64_t hash(std::uint64_t v) noexcept {
+    v ^= hash_ror64(v, 25) ^ hash_ror64(v, 50);
+    v *= 0xA24BAED4963EE407UL;
+    v ^= hash_ror64(v, 24) ^ hash_ror64(v, 49);
+    v *= 0x9FB21C651E98DF25UL;
+    return v ^ v >> 28;
 }
 
-template <typename index_at = native_index_t> //
-static void clear_index(index_at& index) {
-    index.clear();
+template <typename scalar_at>
+inline void hash_typed_buffer(hash_index_w_meta_t& index, py::buffer_info const& vector_info) noexcept {
+    char const* vector_data = reinterpret_cast<char const*>(vector_info.ptr);
+    ssize_t vector_dimensions = vector_info.shape[0];
+    ssize_t vector_stride = vector_info.strides[0];
+    std::memset(index.buffer_.data(), 0, index.words_ * sizeof(hash_word_t));
+
+    for (std::size_t i = 0; i != static_cast<std::size_t>(vector_dimensions); ++i) {
+        scalar_at scalar = *reinterpret_cast<scalar_at const*>(vector_data + i * vector_stride);
+        std::uint64_t scalar_hash = hash(scalar);
+        index.buffer_[scalar_hash % index.words_] |= hash_word_t(1) << (scalar_hash % bits_per_hash_word_k);
+    }
+}
+
+void hash_buffer(hash_index_w_meta_t& index, py::buffer vector) {
+    py::buffer_info info = vector.request();
+    if (info.ndim != 1)
+        throw std::invalid_argument("Array can't be multi-dimensional!");
+
+    // https://docs.python.org/3/library/struct.html#format-characters
+    if (info.format == "h" || info.format == "H")
+        return hash_typed_buffer<std::uint16_t>(index, info);
+    else if (info.format == "i" || info.format == "I" || info.format == "l" || info.format == "L")
+        return hash_typed_buffer<std::uint32_t>(index, info);
+    else if (info.format == "q" || info.format == "Q" || info.format == "n" || info.format == "N")
+        return hash_typed_buffer<std::uint64_t>(index, info);
+    else
+        throw std::invalid_argument("Array elements must be 16, 32, or 64 bit hashable integers!");
 }
 
 PYBIND11_MODULE(usearch, m) {
@@ -340,10 +435,7 @@ PYBIND11_MODULE(usearch, m) {
     si.def( //
         "add",
         [](sets_index_t& index, label_t label, py::array_t<set_member_t> set, bool copy) {
-            if (set.ndim() != 1)
-                throw std::runtime_error("Set can't be multi-dimensional!");
-            if (set.strides(0) != sizeof(set_member_t))
-                throw std::runtime_error("Set can't be strided!");
+            validate_set(set);
             if (index.size() + 1 >= index.capacity())
                 index.reserve(ceil2(index.size() + 1));
             auto proxy = set.unchecked<1>();
@@ -359,12 +451,13 @@ PYBIND11_MODULE(usearch, m) {
     si.def( //
         "search",
         [](sets_index_t& index, py::array_t<set_member_t> set, std::size_t count) -> py::array_t<label_t> {
+            validate_set(set);
             auto proxy = set.unchecked<1>();
             auto view = set_view_t{proxy.data(0), static_cast<std::size_t>(proxy.shape(0))};
-            auto labels_py = py::array_t<label_t>({static_cast<ssize_t>(count)});
+            auto labels_py = py::array_t<label_t>(py_shape_t{static_cast<ssize_t>(count)});
             auto labels_proxy = labels_py.mutable_unchecked<1>();
             auto found = index.search(view, count, &labels_proxy(0), nullptr, 0);
-            labels_py.resize({static_cast<ssize_t>(found)});
+            labels_py.resize(py_shape_t{static_cast<ssize_t>(found)});
             return labels_py;
         },
         py::arg("set"),       //
@@ -379,4 +472,55 @@ PYBIND11_MODULE(usearch, m) {
     si.def("save", &save_index<sets_index_t>, py::arg("path"));
     si.def("load", &load_index<sets_index_t>, py::arg("path"));
     si.def("view", &view_index<sets_index_t>, py::arg("path"));
+    si.def("clear", &clear_index<sets_index_t>);
+
+    auto hi = py::class_<hash_index_w_meta_t>(m, "HashIndex");
+
+    hi.def(                                                                //
+        py::init(&make_hash_index),                                        //
+        py::kw_only(),                                                     //
+        py::arg("bits"),                                                   //
+        py::arg("capacity") = 0,                                           //
+        py::arg("connectivity") = config_t::connectivity_default_k,        //
+        py::arg("expansion_add") = config_t::expansion_add_default_k,      //
+        py::arg("expansion_search") = config_t::expansion_search_default_k //
+    );
+
+    hi.def( //
+        "add",
+        [](hash_index_w_meta_t& index, label_t label, py::buffer array) {
+            if (index.size() + 1 >= index.capacity())
+                index.reserve(ceil2(index.size() + 1));
+            hash_buffer(index, array);
+            index.add(label, index.buffer(), 0);
+        },                //
+        py::arg("label"), //
+        py::arg("array")  //
+    );
+
+    hi.def( //
+        "search",
+        [](hash_index_w_meta_t& index, py::buffer array, std::size_t count) -> py::array_t<label_t> {
+            if (index.size() + 1 >= index.capacity())
+                index.reserve(ceil2(index.size() + 1));
+            hash_buffer(index, array);
+            auto labels_py = py::array_t<label_t>(py_shape_t{static_cast<ssize_t>(count)});
+            auto labels_proxy = labels_py.mutable_unchecked<1>();
+            auto found = index.search(index.buffer(), count, &labels_proxy(0), nullptr, 0);
+            labels_py.resize(py_shape_t{static_cast<ssize_t>(found)});
+            return labels_py;
+        },
+        py::arg("array"),     //
+        py::arg("count") = 10 //
+    );
+
+    hi.def("__len__", &hash_index_w_meta_t::size);
+    hi.def_property_readonly("size", &hash_index_w_meta_t::size);
+    hi.def_property_readonly("connectivity", &hash_index_w_meta_t::connectivity);
+    hi.def_property_readonly("capacity", &hash_index_w_meta_t::capacity);
+
+    hi.def("save", &save_index<hash_index_w_meta_t>, py::arg("path"));
+    hi.def("load", &load_index<hash_index_w_meta_t>, py::arg("path"));
+    hi.def("view", &view_index<hash_index_w_meta_t>, py::arg("path"));
+    hi.def("clear", &clear_index<hash_index_w_meta_t>);
 }
