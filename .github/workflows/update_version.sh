502e9d531906bd5bcdfb61f21c2052ffef88e93c
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-05-18 17:25:49 -0700

    Docs: Citation file

--- .github/workflows/update_version.sh
+++ .github/workflows/update_version.sh
@@ -2,5 +2,6 @@
 
 echo $1 > VERSION && 
     sed -i "s/\"version\": \".*\"/\"version\": \"$1\"/" package.json &&
+    sed -i "s/^version: .*/version: $1/" CITATION.cff &&
     sed -i "s/version = \".*\"/version = \"$1\"/" Cargo.toml &&
     sed -i "s/version = '.*'/version = '$1'/" conanfile.py
