cabcd9eba788b5fb4e4352abda6ce52481cf14e2
Darvin Harutyunyan <12006323+DarvinHarutyunyan@users.noreply.github.com>
2023-08-25 19:08:27 +0400

    Fix: JS test of USearch WASM

--- wasm/test.js
+++ wasm/test.js
@@ -1,5 +1,12 @@
-const Module = require('./usearch-wasm.js');
+var assert = require('assert');
+var module = require('./usearch-wasm.js');
 
-var USearch = new Module.UsearchWasm('usearch.wasm', "");
+var error = ""
+var usearch = new module.UsearchWasm('usearch.wasm', error);
 
-console.log(`USearch`);
+var index = usearch.init({ metricKind: "usearch_metric_ip_k", quantization: "scalar-f32-k", dimensions: 2, connectivity: 16, expansionAdd: 40, expansionSearch: 16 });
+assert.equal(usearch.connectivity(index, error), 16n, 'connectivity should be 16');
+assert.equal(usearch.dimensions(index, error), 2n, 'dimensions should be 2');
+assert.equal(usearch.size(index, error), 0n, 'initial size should be 0');
+
+console.log(`JavaScript WASM test passed!`);
