74814c90be5954556f1853c15f093df5b41dd3c0
Gurgen Yegoryan <21982202+gurgenyegoryan@users.noreply.github.com>
2023-08-31 10:40:05 +0400

    Make: Get architecture in script

--- winlibinstaller.bat
+++ winlibinstaller.bat
@@ -1,33 +1,33 @@
 @echo off
 
-rem Set the version number.
-set version=v2.1.0
+rem Set usearch release version.
+set version=v2.0.2
 
-echo Get library version!
-timeout /t 2
+rem Get Windows architecture
+for /f "tokens=2 delims= " %%a in ('wmic OS get OSArchitecture ^| find "64"') do set "arch=x64"
+if "%arch%"=="" set "arch=x86"
+
+echo Split library version from release version!
+timeout /t 1
 rem Split the first 'v' from the version number.
 for /f "tokens=1 delims=v" %%a in ('echo %version%') do (
-    set first_token=%%a
+    set lib_version=%%a
 )
 
-rem Save the first token in the lib_version variable.
-set lib_version=%first_token%
-echo Getting library version is complete!
-
-echo Download usearch archive
+echo Download usearch library archive!
 rem Download the file.
 timeout /t 2
-curl -LO https://github.com/unum-cloud/usearch/releases/download/%version%/usearch_windows_%lib_version%.tar
+curl -LO https://github.com/gurgenyegoryan/usearch/releases/download/%version%/usearch_windows_%arch%_%lib_version%.tar
 echo Download is complete!
 
 echo Extract archive!
 rem Extract archive.
 timeout /t 1
-tar -xf usearch_windows_%lib_version%.tar
+tar -xf usearch_windows_%arch%_%lib_version%.tar
 echo Extract is complete!
 
 rem Remove the archive.
-del usearch_windows_%lib_version%.tar
+del usearch_windows_%arch%_%lib_version%.tar
 
 echo Extraction complete and downloaded .tar file removed.
 timeout /t 1
