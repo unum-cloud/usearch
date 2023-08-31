65bcb6b75f6b7b5612b049187d917d8dc544df39
Gurgen Yegoryan <21982202+gurgenyegoryan@users.noreply.github.com>
2023-08-31 15:31:28 +0400

    Refactor: Dont update .bat version

--- .github/workflows/update_version.sh
+++ .github/workflows/update_version.sh
@@ -11,5 +11,4 @@ echo $1 > VERSION &&
     sed -i "s/<version>[0-9]\+\.[0-9]\+\.[0-9]\+/<version>$1/" README.md &&
     sed -i "s/version = {[0-9]\+\.[0-9]\+\.[0-9]\+}/version = {$1}/" README.md &&
     sed -i "s/>[0-9]\+\.[0-9]\+\.[0-9]\+<\/Version>/>$1<\/Version>/" ./csharp/nuget/nuget-package.props &&
-    sed -i "s/version=\".*\"/version=\"$1\"/" wasmer.toml &&
-    sed -i "s/set version=.*/set version=v$1/" winlibinstaller.bat
+    sed -i "s/version=\".*\"/version=\"$1\"/" wasmer.toml
