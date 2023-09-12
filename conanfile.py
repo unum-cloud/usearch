80b3fd4579a1fd001d602dd56b1ada7aea610cf6
Mesrop Gevorgyan <79257290+mgevor@users.noreply.github.com>
2023-09-12 17:28:05 +0400

    Build: Conan file updated

--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,26 @@
 from conans import ConanFile
 
 
+required_conan_version = ">=1.53.0"
+
+
 class USearchConan(ConanFile):
 
-    name = 'usearch'
+    name = "usearch"
     version = '2.4.0'
-    license = 'Apache 2.0'
-    url = 'https://github.com/conan-io/conan-center-index'
-    description = 'Smaller & Faster Single-File Vector Search Engine from Unum'
-    homepage = 'https://github.com/unum-cloud/usearch'
-    topics = 'vector-search'
+    license = "Apache-2.0"
+    description = "Smaller & Faster Single-File Vector Search Engine from Unum"
+    homepage = "https://github.com/unum-cloud/usearch"
+    topics = ("search", "vector", "simd")
+    settings = "os", "arch", "compiler", "build_type"
+    url = "https://github.com/conan-io/conan-center-index"
+    package_type = "header-library"
 
     # No settings/options are necessary, this is header only
     # Potentially add unit-tests in the future:
     # https://docs.conan.io/1/howtos/header_only.html#with-unit-tests
-    exports_sources = 'include/*'
+    exports_sources = "include/*"
     no_copy_source = True
 
     def package(self):
-        self.copy('*.h')
+        self.copy("*.h")
