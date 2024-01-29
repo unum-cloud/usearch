167f48c6764df59ba36fb0985b87077dfc39023e
Narek Galstyan <narekg@berkeley.edu>
2024-01-29 23:07:57 +0000

    Avoid including simsimd before index_plugins
    index_plugins configures simsimd and when simsimd is included
    before this configuration gets a chance to run during compilation,
    simsimd.h may be misconfigured
    
    In particular, index_plugins propagates USEARCH_FP16LIB cmake
    options as !SIMSIMD_NATIVE_FP16 (see [1]) and if simsimd.h
    is included before index_plugins, wrong value of
    SIMSIMD_NATIVE_FP16 may be chosen
    
    [1]:
    https://github.com/unum-cloud/usearch/blob/ce54b814a8a10f4c0c32fee7aad9451231b63f75/include/usearch/index_plugins.hpp#L50

--- cpp/bench.cpp
+++ cpp/bench.cpp
@@ -42,10 +42,6 @@
 #include <omp.h> // `omp_set_num_threads()`
 #endif
 
-#if USEARCH_USE_SIMSIMD
-#include <simsimd/simsimd.h>
-#endif
-
 #include <usearch/index_dense.hpp>
 
 using namespace unum::usearch;
