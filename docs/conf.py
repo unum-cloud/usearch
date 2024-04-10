f4df7aceda7caa45ab01f65413fd0eaa20329345
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-04-10 02:25:24 +0000

    Docs: TypeScript instructions

--- docs/conf.py
+++ docs/conf.py
@@ -52,4 +52,9 @@ html_baseurl = "/docs/usearch/"
 breathe_projects = {"USearch": "../build/xml"}
 breathe_default_project = "USearch"
 
+# To switch to TypeScript, uncomment the following lines:
+#
+#   js_language = "typescript"
+#   js_source_path = "../javascript/usearch.ts"
+#   jsdoc_config_path = "../javascript/tsconfig-cjs.json"
 js_source_path = "../javascript/dist/cjs/usearch.js"
