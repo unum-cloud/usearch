8490f1d03826d23d3fca25d6b180b3fbb68e6f4b
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-05-07 18:04:49 -0700

    Fix: Misc warnings & compilation issues

--- java/cloud/unum/usearch/cloud_unum_usearch_Index.cpp
+++ java/cloud/unum/usearch/cloud_unum_usearch_Index.cpp
@@ -39,7 +39,7 @@ JNIEXPORT jlong JNICALL Java_cloud_unum_usearch_Index_c_1create( //
         }
 
         index_dense_t index = index_dense_t::make(metric, config);
-        if (!index.reserve(static_cast<std::size_t>(capacity))) {
+        if (!index.try_reserve(static_cast<std::size_t>(capacity))) {
             jclass jc = (*env).FindClass("java/lang/Error");
             if (jc)
                 (*env).ThrowNew(jc, "Failed to reserve desired capacity!");
