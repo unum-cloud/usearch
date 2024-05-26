78c677acb127d79695cf276f3569caf70ff97183
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-05-25 23:38:50 -0700

    Make: Manually bump version to 2.13.0
    We can't yet rely on the SemVer tool
    https://github.com/semantic-release/release-notes-generator/issues/633#issuecomment-2132077423

--- include/usearch/index.hpp
+++ include/usearch/index.hpp
@@ -8,7 +8,7 @@
 #define UNUM_USEARCH_HPP
 
 #define USEARCH_VERSION_MAJOR 2
-#define USEARCH_VERSION_MINOR 12
+#define USEARCH_VERSION_MINOR 13
 #define USEARCH_VERSION_PATCH 0
 
 // Inferring C++ version
