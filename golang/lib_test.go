580457707cf7e10cfe9e8e5d745d56fea403df79
Mark Reed <markreed99@gmail.com>
2024-06-02 07:10:12 -0700

    Add: the missing APIs for go.  Closes #409

--- golang/lib_test.go
+++ golang/lib_test.go
@@ -1,9 +1,9 @@
 package usearch
 
 import (
+	"math"
 	"runtime"
 	"testing"
-	"math"
 )
 
 func TestUSearch(t *testing.T) {
@@ -34,10 +34,35 @@ func TestUSearch(t *testing.T) {
 			t.Fatalf("Expected size to be 0, got %d", found_len)
 		}
 
+		found_len, err = ind.SerializedLength()
+		if err != nil {
+			t.Fatalf("Failed to retrieve serialized length: %s", err)
+		}
+		if found_len != 112 {
+			t.Fatalf("Expected serialized length to be 112, got %d", found_len)
+		}
+
+		mem, err := ind.MemoryUsage()
+		if err != nil {
+			t.Fatalf("Failed to retrieve serialized length: %s", err)
+		}
+		if mem != 0 {
+			t.Fatalf("Expected the empty index memory usage to be 0, got %d", mem)
+		}
+
 		err = ind.Reserve(100)
 		if err != nil {
 			t.Fatalf("Failed to reserve capacity: %s", err)
 		}
+
+		s, err := ind.HardwareAcceleration()
+		if err != nil {
+			t.Fatalf("Failed to retrieve hardware acceleration: %s", err)
+		}
+		if s == "" {
+			t.Fatalf("An empty string was returned from HardwareAcceleration")
+		}
+
 	})
 
 	t.Run("Test Insertion", func(t *testing.T) {
@@ -104,5 +129,91 @@ func TestUSearch(t *testing.T) {
 		if keys[0] != 42 || math.Abs(float64(distances[0])) > tolerance {
 			t.Fatalf("Expected result 42 with distance 0, got key %d with distance %f", keys[0], distances[0])
 		}
+
+        // TODO: Add exact search
+	})
+
+	t.Run("Test Save and Load", func(t *testing.T) {
+		dim := uint(128)
+		conf := DefaultConfig(dim)
+		ind, err := NewIndex(conf)
+		if err != nil {
+			t.Fatalf("Failed to construct the index: %s", err)
+		}
+		defer ind.Destroy()
+		ind2, err := NewIndex(conf)
+		if err != nil {
+			t.Fatalf("Failed to construct the index: %s", err)
+		}
+		defer ind2.Destroy()
+		indView, err := NewIndex(conf)
+		if err != nil {
+			t.Fatalf("Failed to construct the index: %s", err)
+		}
+		defer indView.Destroy()
+
+		err = ind.Reserve(100)
+		if err != nil {
+			t.Fatalf("Failed to reserve capacity: %s", err)
+		}
+
+		vec := make([]float32, dim)
+        for i := uint(0); i < dim; i++ {
+		    vec[i] = float32(i) + 0.2
+		    err = ind.Add(uint64(i), vec)
+		    if err != nil {
+			    t.Fatalf("Failed to insert: %s", err)
+		    }
+        }
+
+		ind_length, err := ind.Len()
+		if err != nil {
+			t.Fatalf("Failed to retrieve size: %s", err)
+		}
+
+        // TODO: Add invalid save and loads?
+        buffer_size := uint(1*1024*1024)
+        buf := make([]byte, buffer_size)
+		err = ind.SaveBuffer(buf, buffer_size)
+		if err != nil {
+			t.Fatalf("Failed to save the index to a buffer: %s", err)
+		}
+
+		err = ind2.LoadBuffer(buf, buffer_size)
+		if err != nil {
+			t.Fatalf("Failed to load the index from a buffer: %s", err)
+		}
+
+		ind2_length, err := ind2.Len()
+		if err != nil {
+			t.Fatalf("Failed to retrieve size: %s", err)
+		}
+		if ind_length != ind2_length {
+			t.Fatalf("Loaded index length %d doesn't match original of %d ", ind2_length, ind_length)
+		}
+        // TODO: Check some values
+
+		err = indView.ViewBuffer(buf, buffer_size)
+		if err != nil {
+			t.Fatalf("Failed to load the view from a buffer: %s", err)
+		}
+
+		indView_length, err := indView.Len()
+		if err != nil {
+			t.Fatalf("Failed to retrieve size: %s", err)
+		}
+		if ind_length != indView_length {
+			t.Fatalf("Loaded view length %d doesn't match original of %d ", indView_length, ind_length)
+		}
+
+		conf, err = MetadataBuffer(buf, buffer_size)
+		if err != nil {
+			t.Fatalf("Failed to load the metadata from a buffer: %s", err)
+		}
+		if conf != ind.config {
+			t.Fatalf("Loaded metadata doesn't match the index metadata")
+		}
+
+        // TODO: Check file save/load/metadata
 	})
 }
