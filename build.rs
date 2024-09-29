f1c158f701431a368db2f1819e301eebac25267b
CCnut <C.C.nut@outlook.com>
2024-09-29 23:19:21 +0800

    Make: Android CI build and test

--- build.rs
+++ build.rs
@@ -25,26 +25,8 @@ fn main() {
 
     // Define all possible SIMD targets as 1
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
-    let flags_to_try = match target_arch.as_str() {
-        "arm" | "aarch64" => vec![
-            "SIMSIMD_TARGET_SVE_BF16",
-            "SIMSIMD_TARGET_SVE_F16",
-            "SIMSIMD_TARGET_SVE_I8",
-            "SIMSIMD_TARGET_SVE",
-            "SIMSIMD_TARGET_NEON_BF16",
-            "SIMSIMD_TARGET_NEON_F16",
-            "SIMSIMD_TARGET_NEON_I8",
-            "SIMSIMD_TARGET_NEON",
-        ],
-        _ => vec![
-            "SIMSIMD_TARGET_SAPPHIRE",
-            "SIMSIMD_TARGET_GENOA",
-            "SIMSIMD_TARGET_ICE",
-            "SIMSIMD_TARGET_SKYLAKE",
-            "SIMSIMD_TARGET_HASWELL",
-        ],
-    };
 
+    let mut flags_to_try;
     if cfg!(feature = "simsimd") {
         build
             .file("simsimd/c/lib.c")
@@ -52,23 +34,40 @@ fn main() {
             .define("SIMSIMD_DYNAMIC_DISPATCH", "1")
             .define("SIMSIMD_NATIVE_BF16", "0")
             .define("SIMSIMD_NATIVE_F16", "0");
-
-        for flag in &flags_to_try {
-            build.define(flag, "1");
-        }
+        flags_to_try = match target_arch.as_str() {
+            "arm" | "aarch64" => vec![
+                "SIMSIMD_TARGET_NEON",
+                "SIMSIMD_TARGET_NEON_I8",
+                "SIMSIMD_TARGET_NEON_F16",
+                "SIMSIMD_TARGET_NEON_BF16",
+                "SIMSIMD_TARGET_SVE",
+                "SIMSIMD_TARGET_SVE_I8",
+                "SIMSIMD_TARGET_SVE_F16",
+                "SIMSIMD_TARGET_SVE_BF16",
+            ],
+            _ => vec![
+                "SIMSIMD_TARGET_HASWELL",
+                "SIMSIMD_TARGET_SKYLAKE",
+                "SIMSIMD_TARGET_ICE",
+                "SIMSIMD_TARGET_GENOA",
+                "SIMSIMD_TARGET_SAPPHIRE",
+            ],
+        };
     } else {
         build.define("USEARCH_USE_SIMSIMD", "0");
+        flags_to_try = vec![];
     }
 
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
     // Conditional compilation depending on the target operating system.
-    if cfg!(target_os = "linux") {
+    if target_os == "linux" || target_os == "android" {
         build
             .flag_if_supported("-std=c++17")
             .flag_if_supported("-O3")
             .flag_if_supported("-ffast-math")
             .flag_if_supported("-fdiagnostics-color=always")
             .flag_if_supported("-g1"); // Simplify debugging
-    } else if cfg!(target_os = "macos") {
+    } else if target_os == "macos" {
         build
             .flag_if_supported("-mmacosx-version-min=10.15")
             .flag_if_supported("-std=c++17")
@@ -76,7 +75,7 @@ fn main() {
             .flag_if_supported("-ffast-math")
             .flag_if_supported("-fcolor-diagnostics")
             .flag_if_supported("-g1"); // Simplify debugging
-    } else if cfg!(target_os = "windows") {
+    } else if target_os == "windows" {
         build
             .flag_if_supported("/std:c++17")
             .flag_if_supported("/O2")
@@ -90,25 +89,35 @@ fn main() {
             .define("_ALLOW_POINTER_TO_CONST_MISMATCH", None);
     }
 
-    let mut result = build.try_compile("usearch");
-    if result.is_err() {
-        print!("cargo:warning=Failed to compile with all SIMD backends...");
-        for flag in flags_to_try {
-            build.define(flag, "0");
-            result = build.try_compile("usearch");
-            if result.is_err() {
+    let base_build = build.clone();
+
+    let mut pop_flag = None;
+    loop {
+        let mut sub_build = base_build.clone();
+        for flag in &flags_to_try {
+            sub_build.define(flag, "1");
+        }
+        let result = sub_build.try_compile("usearch");
+        if result.is_err() {
+            if let Some(flag) = pop_flag {
                 println!(
-                    "cargo:warning=Failed to compile after disabling {}, trying next configuration...",
+                    "cargo:warning=Failed to compile after disabling {:?}, trying next configuration...",
                     flag
                 );
             } else {
-                break;
+                if !flags_to_try.is_empty() {
+                    print!("cargo:warning=Failed to compile with all SIMD backends...");
+                }
             }
+
+            pop_flag = flags_to_try.pop();
+            if pop_flag.is_none() {
+                result.unwrap();
+            }
+        } else {
+            break;
         }
     }
-
-    // Ensure one build has been successful
-    result.unwrap();
 
     println!("cargo:rerun-if-changed=rust/lib.rs");
     println!("cargo:rerun-if-changed=rust/lib.cpp");
