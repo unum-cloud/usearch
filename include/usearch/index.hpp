6a0e2ee3db127a32c9d4d6f4b1b20eeb5b71aef1
TinySemVer <tinysemver@ashvardanian.com>
2024-10-29 21:09:10 +0000

    Release: v2.16.0 [skip ci]

--- include/usearch/index.hpp
+++ include/usearch/index.hpp
@@ -8,8 +8,8 @@
 #define UNUM_USEARCH_HPP
 
 #define USEARCH_VERSION_MAJOR 2
-#define USEARCH_VERSION_MINOR 15
-#define USEARCH_VERSION_PATCH 3
+#define USEARCH_VERSION_MINOR 16
+#define USEARCH_VERSION_PATCH 0
 
 // Inferring C++ version
 // https://stackoverflow.com/a/61552074
